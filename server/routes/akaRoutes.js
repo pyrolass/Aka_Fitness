dec74262d61852e9fd52c0e47e732c78bcb01568
Las Rock <crowclaw935@gmail.com>
2021-10-03 01:44:18 +0300

    workout query added by las

--- server/routes/akaRoutes.js
+++ server/routes/akaRoutes.js
@@ -3,8 +3,8 @@ const router = express.Router();
 const akaController = require('./../controllers/akaController');
 
 
-// router.route('/get-favorite').get(akaController.getFavotiteData);
+router.route('/get-favorite').get(akaController.getFavotiteData);
 router.route('/:workout').get(akaController.getWorkout);
-// router.route('/').get(akaController.getAllData);
+router.route('/').get(akaController.getAllData);
 
 module.exports = router;
